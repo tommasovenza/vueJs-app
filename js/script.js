7636d254f832a7968a0907e1ba3ad2bb26b821a7
Tommaso <tommasovenza@gmail.com>
2021-03-21 17:41:05 +0100

    added messages with javascripts and new photos pictures

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-// $(document).ready(function( ) {
+$(document).ready(function( ) {
    
-// });
+
+    $(document).on('click', '#icon', function() {
+        var clone = $('#template').children().clone();
+        console.log(clone);
+
+        var inputMessage = $('#message').val();
+        console.log(inputMessage);
+
+        $('.message-table').append(clone);
+        $('.message-table p').last().text(inputMessage);
+        
+        $('#message').val('');
+
+    });
+});
