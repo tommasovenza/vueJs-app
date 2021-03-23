8d29572d4256cb15120561b1ab63a12b835a986e
Tommaso <tommasovenza@gmail.com>
2021-03-23 15:28:59 +0100

    hide other messages at click event

--- js/script.js
+++ js/script.js
@@ -8,6 +8,19 @@ $(document).ready(function( ) {
         setTimeout(answerMessage, 1000);
     });
 
+    $(document).on('click', '.fa-chevron-down.active', function() {
+        // show dropdown
+        $(this).siblings('.with-drop').toggleClass('active');
+
+        // hide other dropdown at every click on chevron in the app
+        $(this).parent().siblings('.message').find('.with-drop').removeClass('active');
+        
+    });
+    // event deleting click
+    $(document).on('click', '.delete', function() {
+        $(this).parents('.message').remove();
+    });
+
     // event press on keyboard for writing a message 
     $(document).keyup(function(event) {
         // take input value
@@ -37,14 +50,6 @@ $(document).ready(function( ) {
     });
     $(document).on('mouseleave', '.message.received', function() {
         $(this).find('.fa-chevron-down').removeClass('active');
-    });
-
-    // event click on chevron down for deleting a message
-    $(document).on('click','.message.sent i',function() {
-        $(this).parents('.message').remove();
-    });
-    $(document).on('click','.message.received i', function() {
-        $(this).parents('.message').remove();
     });
 });
 
@@ -125,4 +130,3 @@ function searchContacts() {
     });
 
 }
- 
